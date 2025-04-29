c72bdbb72d87f3ca8a3468d8de931680ad30c2da
maryam-amn <Maryam.Aman@jobtrek.ch>
2025-04-29 14:49:12 +0200

    docs: add the database into the image directory

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Homepage</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-page/style.css">
 </head>
 <body>
 <div class="header">
@@ -30,7 +30,7 @@
 
     <div class="font-post-homepage">
         <h4>La natation</h4>
-        <img src="image_home.jpg">
+        <img src="image/image_home.jpg">
         <p class="text-paragraphe">La nage est une activité physique pratiquée depuis l’Antiquité, à la fois pour se
             déplacer dans l’eau,
             se détendre et entretenir sa santé.
@@ -41,7 +41,7 @@
 
     <div class="font-post-homepage">
         <h4>Le basketball</h4>
-        <img src="img_post.jpg">
+        <img src="image/img_post.jpg">
 
         <p class="text-paragraphe">Rapidité, précision, esprit d’équipe : le basketball ne laisse pas de place à
             l’ennui.
@@ -53,7 +53,7 @@
 
     <div class="font-post-homepage">
         <h4 class="text-paragraphe">Le jardinage</h4>
-        <a href="Detail_post.php"> <img src="login_img.jpg"> </a>
+        <a href="Detail_post.php"> <img src="image/login_img.jpg"> </a>
         <p class="text-paragraphe">Le jardinage, c’est un retour à l’essentiel. Chaque graine plantée, chaque fleur qui
             éclot
             est un petit miracle de la nature. Prendre soin d’un jardin, c’est prendre soin de soi.
